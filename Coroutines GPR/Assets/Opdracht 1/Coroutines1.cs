902354668be034ba5f6f52dc33d862f6d47eb67c
Cas de Graaff <casdegraaff@gmail.com>
2021-02-14 15:31:45 +0100

    Opdracht 1 done

---
+++ Coroutines GPR/Assets/Opdracht 1/Coroutines1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coroutines1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Fade"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Fade()
+    {
+        Debug.Log("Coroutine update");
+        yield return new WaitForSeconds(.5f);
+        Debug.Log("Coroutine update 2");
+        yield return new WaitForSeconds(.5f);
+        Debug.Log("Coroutine update 3");
+        yield return new WaitForSeconds(.5f);
+    }
+}
